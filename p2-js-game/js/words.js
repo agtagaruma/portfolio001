70f38d180158a7460dddbb66c408b5c71266717d
Antonio Tagaruma <agtagaruma05@gmail.com>
2022-10-03 08:03:46 +0800

    fix: minor adjustments

--- p2-js-game/js/words.js
+++ p2-js-game/js/words.js
@@ -14,52 +14,60 @@ let wordList = [
     {
         word: "chile",
         hint: "🌶️"
-    },{
+    },
+    {
         word: "cuba",
         hint: "🧊 + 🅰️"
-    },{
+    },
+    {
         word: "germany",
         hint: "🦠 + 💵"
-    },{
+    },
+    {
         word: "iceland",
         hint: "👀 + 🏝️"
-    },{
+    },
+    {
         word: "taiwan",
         hint: "👔 + 1️⃣"
-    },{
-        word: "chicken",
-        hint: "🐤 + N"
-    },{
+    },
+    {
         word: "china",
         hint: "⛓️ + 🅰️"
-    },{
+    },
+    {
         word: "madagascar",
         hint: "😠 + ⛽ + 🛻"
-    },{
+    },
+    {
         word: "philippines",
         hint: "💊 + 💋 + 📌📌"
-    },{
+    },
+    {
         word: "qatar",
         hint: "🐈‍⬛ + ®️"
-    },{
+    },
+    {
         word: "serbia",
         hint: "S + 👂 + 🐝 + 🅰️"
-    },{
+    },
+    {
         word: "haiti",
         hint: "👋 + 🍵"
-    },{
+    },
+    {
         word: "turkey",
         hint: "2️⃣ + R + 🗝️"
-    },{
+    },
+    {
         word: "botswana",
         hint: "⛵ + 🦢 + 🅰️"
-    },{
-        word: "new-zealand",
-        hint: "📰 + 🌊 + 🏝️"
-    },{
+    },
+    {
         word: "armenia",
         hint: "💪 + 👬 + IA"
-    },{
+    },
+    {
         word: "oman",
         hint: "0️⃣ + 🙍‍♂️"
     },
