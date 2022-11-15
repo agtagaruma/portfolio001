358bf6868082d5b1a91d3bd8cf1f433915bdf8d3
agtagaruma <agtagaruma05@gmail.com>
2022-11-15 23:06:11 +0800

    fix: updated Accomodations model

--- p4-node-app/models/Accommodations.js
+++ p4-node-app/models/Accommodations.js
@@ -3,12 +3,11 @@ const mongoose = require('mongoose');
 const AccommodationSchema = new mongoose.Schema({
     accommodation_name: String,
     accommodation_details: String,
+    accommodation_checkIn: Date,
+    accommodation_checkOut: Date,
     accommodation_location: String,
     accommodation_price: Number,
-    accommodation_reviews: String,
-    accommodation_rank: Number,
-    accommodation_terms: String,
-    
+    accommodation_terms: String    
 });
 
 module.exports = mongoose.model('Accommodation', AccommodationSchema);
